c6754bdd854dac853bf04d34f131f3f5b5ab6af8
Joshua-Jenkins <joshjenkins6@yahoo.com>
2019-03-10 05:49:16 -0500

    save tokens in cookies added

--- services/auth0.js
+++ services/auth0.js
@@ -1,4 +1,5 @@
 import auth0 from "auth0-js";
+import Cookies from "js-cookie";
 
 class Auth0 {
   constructor() {
@@ -10,7 +11,9 @@ class Auth0 {
       scope: "openid profile"
     });
     this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
     this.handleAuthenication = this.handleAuthenication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
   }
 
   handleAuthenication() {
@@ -27,12 +30,38 @@ class Auth0 {
     });
   }
 
-  setSession() {
-    // save tokens!!
+  setSession(authResult) {
+    // set the time that the access token expire
+    const expiresAt = JSON.stringify(
+      authResult.expireIn * 1000 + new Date().getTime()
+    );
+
+    //localStorage.setItem("access_token", authResult.accessToken);
+
+    Cookies.set("user", authResult.idTokenPayload);
+    Cookies.set("jwt", authResult.idToken);
+    Cookies.set("expiresAt", expiresAt);
+    // navigate to the home route
+  }
+
+  logout() {
+    Cookies.remove("user");
+    Cookies.remove("jwt");
+    Cookies.remove("expiresAt");
+
+    this.auth0.logout({
+      returnTo: "",
+      clientID: "vjcAT2iJ0q9DLj3x4u5DXKYsI2TxvHcn"
+    });
   }
 
   login() {
     this.auth0.authorize();
+  }
+
+  isAuthenticated() {
+    const expiresAt = Cookies.getJSON("expiresAt");
+    return new Date().getTime() < expiresAt;
   }
 }
 
