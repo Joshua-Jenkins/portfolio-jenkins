4215b457c9c28bb41b3408706b9c63cca87f536e
Joshua-Jenkins <joshjenkins6@yahoo.com>
2019-03-10 01:35:17 -0600

    app cleanup and CSS

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import { Button, Container } from "reactstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 class Index extends React.Component {
   render() {
     return (
       <BaseLayout>
-        <Container>
-          <Button color="danger">Danger</Button>
-        </Container>
+        <Container />
       </BaseLayout>
     );
   }
