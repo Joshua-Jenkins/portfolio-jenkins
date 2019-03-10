686b3126c6c2cfec217bcfb55b4a4be5975b7d77
Joshua-Jenkins <joshjenkins6@yahoo.com>
2019-03-10 01:19:41 -0600

    added fonts

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
-import { Button } from "reactstrap";
+import { Button, Container } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class Index extends React.Component {
   render() {
     return (
       <BaseLayout>
-        <h1>I am Index Page from Class Component</h1>
-        <Button color="danger">Danger</Button>
+        <Container>
+          <Button color="danger">Danger</Button>
+        </Container>
       </BaseLayout>
     );
   }
