6d0e954098ad6a17156e22f8ccddebf15b46fad4
Joshua-Jenkins <joshjenkins6@yahoo.com>
2019-03-10 01:52:52 -0600

    added index page styling

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -2,10 +2,13 @@ import React from "react";
 import Header from "../shared/Header";
 
 const BaseLayout = props => {
+  const { className, children } = props;
   return (
-    <div>
+    <div className="layout-container">
       <Header />
-      {props.children}
+      <main className={`cover ${className}`}>
+        <div className="wrapper">{children}</div>
+      </main>
     </div>
   );
 };
